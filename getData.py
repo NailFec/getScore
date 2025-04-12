805536734d53c146b4ac9a54789c7a6c4521e98f
NailFec <nail-17@outlook.com>
2025-04-12 21:18:35 +0800

    add first excel line in getData.py

--- getData.py
+++ getData.py
@@ -1,19 +1,20 @@
 import openpyxl
 import pandas as pd
 
-from basic import get_cookie, request2, analyze_string
+from basic import get_cookie, request1_2, request2, analyze_string
 
 df = pd.read_excel("2023stu-all.xlsx")
 wb = openpyxl.Workbook()
 ws = wb.active
 
-# [[NailFecMODIFY]]
-selval = 227
-allsubsn = ['876', '895', '891', '884', '879', '882']
-
 ws.cell(row=1, column=1, value="ID")
 ws.cell(row=1, column=2, value="学号")
 ws.cell(row=1, column=3, value="姓名")
+
+# [[NailFecMODIFY]]
+selval = 227
+subresult = request1_2(selval)
+allsubsn = [item["exasubSN"] for item in subresult]
 
 for iPerson, row in df.iterrows():
     iPerson = int(iPerson)
@@ -24,7 +25,6 @@ for iPerson, row in df.iterrows():
     text = get_cookie(sNum, cNum, name)
 
     nsub = len(allsubsn)
-    allsubsn = ",".join(allsubsn)
     result = request2(selval, allsubsn, text)
 
     ws.cell(row=iPerson + 2, column=1, value=sNum)
@@ -35,5 +35,14 @@ for iPerson, row in df.iterrows():
     for i, data in enumerate(allData):
         ws.cell(row=iPerson + 2, column=i + 4, value=data)
     print(allData)
+
+lastp = 0
+for i in range(nsub):
+    pa = result.find("</th><th>", lastp)
+    pb = result.find("</th>", pa + 2)
+    thisData = result[(pa + len("</th><th>")): pb]
+    ws.cell(row=1, column=i + 4, value=thisData)
+    ws.cell(row=1, column=i + 4 + nsub, value=thisData)
+    lastp = pa + 1
 
 wb.save("lsoutput.xlsx")  # [[NailFecMODIFY]]
